58b678136d6698968845c52732a089a6e12f39b7
Adriano Raiano <adriano@raiano.ch>
2025-02-05 15:03:27 +0100

    try manipulation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log('hello')
+window.enableYoutubeHook = function () {
+  document.querySelector("[id-for-custom-script='watch-video']").setAttribute('href', '#')
+};
+window.disableYoutubeHook = function () {
+  document.querySelector("[id-for-custom-script='watch-video']").setAttribute('href', 'https://youtu.be/TFV_vhJs5DY')
+};
+
+(function() {
+  // to do
+})();
